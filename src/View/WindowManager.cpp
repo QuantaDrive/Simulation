ee087ea52f1144ea6ac0020c1f10c1506a8a983f
LSP95 <rvcospam@gmail.com>
2025-02-17 11:38:55 +0100

    - Single Instruction Window aangemaakt. - Wordt nu nog lokaal aan waarden toegewezen.

--- src/View/WindowManager.cpp
+++ src/View/WindowManager.cpp
@@ -5,14 +5,24 @@
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
 #include <iostream>
+#include <bits/algorithmfwd.h>
 
 class WindowManager : public IWindowManager {
 private:
-    GLFWwindow*window = nullptr;
+    GLFWwindow *window = nullptr;
+    int inputX = 0;
+    int inputY = 0;
+    int inputZ = 0;
+    float inputXDegrees = 0;
+    float inputYDegrees = 0;
+    float inputZDegrees = 0;
+    int gripforce = 0;
+    std::string textInput = "";
 
 public:
-    void init(GLFWwindow* existingWindow) override {
+    void init(GLFWwindow *existingWindow) override {
         window = existingWindow;
+
 
         glewExperimental = GL_TRUE;
         if (glewInit() != GLEW_OK) {
@@ -23,31 +33,55 @@ public:
         // Setup ImGui
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
-        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        ImGuiIO &io = ImGui::GetIO();
+        (void) io;
         ImGui_ImplGlfw_InitForOpenGL(window, true);
         ImGui_ImplOpenGL3_Init("#version 330");
 
         std::cout << "Window initialized successfully\n";
     }
 
-    void run() override {
-        bool showWindow = true;
-
+    void singleInstructionWindow(bool showWindow) {
         // Start ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
         // Create an ImGui window
-        ImGui::Begin("Example Window", &showWindow);
-        if (ImGui::Button("Press Me!")) {
-            std::cout << "Button Pressed!\n";
+
+        ImGui::Begin("Single instruction", &showWindow);
+        // Create an input field for ints
+        ImGui::InputInt("X", &inputX);
+        ImGui::InputInt("Y", &inputY);
+        ImGui::InputInt("Z", &inputZ);
+        ImGui::SliderAngle("X Degrees", &inputXDegrees, -360, 360);
+        ImGui::SliderAngle("Y Degrees", &inputYDegrees, -360, 360);
+        ImGui::SliderAngle("Z Degrees", &inputZDegrees, -360, 360);
+        ImGui::SliderInt("Grip Force", &gripforce, 0, 10);
+
+
+
+        // Create a button
+        if (ImGui::Button("show values")) {
+            textInput = "X = " + std::to_string(inputX) +
+                        ", Y = " + std::to_string(inputY) +
+                        ", Z = " + std::to_string(inputZ) +
+                        " X Degrees = " + std::to_string(inputXDegrees) +
+                        " Y Degrees = " + std::to_string(inputYDegrees) +
+                        " Z Degrees = " + std::to_string(inputZDegrees);
         }
+        ImGui::Text(textInput.c_str());
         ImGui::End();
 
         // Rendering
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    }
+
+    void run() override {
+        bool showWindow = true;
+
+        singleInstructionWindow(showWindow);
     }
 
     void close() override {
