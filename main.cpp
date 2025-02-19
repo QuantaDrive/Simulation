4c2dcc8234bbdcb20ca75f6e96c7f55c7f4fd09b
LSP95 <99749763+LSP95@users.noreply.github.com>
2025-02-19 15:45:52 +0100

    - Simulation package en windowmanager merged.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
+
 #include <Simulation/Simulation.h>
+#include "src/View/WindowManager.cpp"
+
+namespace ed = ax::NodeEditor;
+ed::EditorContext *g_Context = nullptr;
+WindowManager windowManager = WindowManager();
 
 int main()
 {
@@ -15,15 +21,26 @@ int main()
 
 	simulation::Mesh model = simulation::Mesh("monkey.obj");
 
+	// Setup Node Editor
+	ed::Config config;
+	config.SettingsFile = "Simple.json";
+	g_Context = ed::CreateEditor(&config);
+
+	// Setup ImGui
+	windowManager.SetupImGui(simulation::window);
 	do
 	{
 		simulation::refresh();
 
         model.render();
+		// Render ImGui UI and Node Editor together
+		windowManager.RenderUI(g_Context);
 	} // Check if the ESC key was pressed or the window was closed
 	while (simulation::needClose());
 
 	simulation::Close();
+	// Cleanup ImGui & Node Editor
+	windowManager.CleanupImGui(g_Context);
 
 	return 0;
 }
