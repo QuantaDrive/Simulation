cb0dc8d0cdb05f84b858ac7f2960bcf324ad5319
LSP95 <99749763+LSP95@users.noreply.github.com>
2025-02-28 11:14:27 +0100

    - Nodes klasse aangevuld en getest.

--- src/Domain/Node.cpp
+++ src/Domain/Node.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "Node.h"
+
+Node::Node(RobotActions::NodeActivation activation, int& nodeId)
+    : id_(nodeId),
+      activation_(activation),
+      nodeId_(nodeId++),
+      nodeInputPinId_(nodeId++),
+      nodeOutputPinId_(nodeId++)
+{
+}
+
+int Node::getId() const
+{
+    return id_;
+}
+
+RobotActions::NodeActivation Node::getActivation() const
+{
+    return activation_;
+}
+
+ed::NodeId Node::getNodeId() const
+{
+    return nodeId_;
+}
+
+ed::PinId Node::getNodeInputPinId() const
+{
+    return nodeInputPinId_;
+}
+
+ed::PinId Node::getNodeOutputPinId() const
+{
+    return nodeOutputPinId_;
+}
+
+
---
+++ src/Domain/NodeActivation.cpp
@@ -0,0 +1,20 @@
+namespace RobotActions {
+    enum class NodeActivation {
+        Wait,
+        RapidMove,
+        LinearMove,
+        DirectMove,
+        Absolute,
+        Relative,
+        Home,
+        EnableMotors,
+        DisableMotors,
+        Loop,
+        Grip,
+        Hold,
+        Action
+    };
+}
+//
+// Created by ComicSansEnfants on 28/02/2025.
+//
